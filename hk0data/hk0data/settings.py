008df896161056b88464bad89d3b94cd6d5fe202
Sammy Fung <sammy@sammy.hk>
2014-03-14 03:39:58 +0800

    fixed sqlite3 file path problem.

--- hk0data/hk0data/settings.py
+++ hk0data/hk0data/settings.py
@@ -9,10 +9,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import os
+sqlfile = os.path.dirname(__file__)+'/../hk0data.sqlite3'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/sfung/Projects/scrapy/hk0test/hk0data/hk0data.sqlite3',                      # Or path to database file if using sqlite3.
+        'NAME': sqlfile,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
--- hk0weather/settings.py
+++ hk0weather/settings.py
@@ -14,8 +14,10 @@ NEWSPIDER_MODULE = 'hk0weather.spiders'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'hk0weather (+http://www.yourdomain.com)'
 
+import os
+
 def setup_django_env(path):
-  import imp, os
+  import imp
   from django.core.management import setup_environ
  
   f, filename, desc = imp.find_module('settings', [path])
@@ -27,7 +29,7 @@ def setup_django_env(path):
   import sys
   sys.path.append(os.path.abspath(os.path.join(path, os.path.pardir)))
  
-setup_django_env('/home/sfung/Projects/scrapy/hk0test/hk0data/hk0data')
+setup_django_env(os.path.dirname(__file__)+'/../hk0data/hk0data')
 
 ITEM_PIPELINES = {
   'hk0weather.pipelines.Hk0RegionalPipeline': 300,
