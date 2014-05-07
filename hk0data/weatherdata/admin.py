6eb6c0bfa62a13ce603681115a28b7ec25589159
Sammy Fung <sammy@sammy.hk>
2014-05-08 03:28:02 +0800

    added checking for duplicated data, and weather data filter by station and wind direction at Django

--- hk0data/weatherdata/admin.py
+++ hk0data/weatherdata/admin.py
@@ -3,6 +3,7 @@ from weatherdata.models import WeatherData
 
 class WeatherDataAdmin(admin.ModelAdmin):
      list_display = ('scraptime', 'reptime', 'station','ename','cname','temperture','humidity','temperturemax','temperturemin','winddirection','windspeed','maxgust')
+     list_filter = ['station', 'winddirection']
 
 admin.site.register(WeatherData, WeatherDataAdmin)
 
--- hk0weather/pipelines.py
+++ hk0weather/pipelines.py
@@ -7,10 +7,14 @@
 #    def process_item(self, item, spider):
 #        return item
 
+from scrapy.exceptions import DropItem
 from weatherdata.models import WeatherData
 
 class Hk0RegionalPipeline(object):
 
   def process_item(self, item, spider):
-    item.save()
+    if not WeatherData.objects.filter(reptime = item['reptime'], station=item['station']):
+      item.save()
+    else:
+      raise DropItem("Data time %s from station %s exists." % (item['reptime'],item['station'])) 
     return item
