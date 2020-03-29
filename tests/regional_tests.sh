facab4e5a089e6ef87cdb49321773bf8785e5730
Sammy Fung <sammy@sammy.hk>
2020-03-29 15:40:55 +0800

    add 9dayforecast test script and update regional test script

--- tests/regional_tests.sh
+++ tests/regional_tests.sh
@@ -1,6 +1,6 @@
-rm -f test.csv
-scrapy crawl regional -t csv -o test.csv --nolog
-line_count=`wc -l test.csv | cut -c 7-8`
+rm -f regional.csv
+scrapy crawl regional -t csv -o regional.csv --nolog
+line_count=`wc -l regional.csv | cut -c 7-8`
 if [ ${line_count} -eq " 0" ]
 then
 	exit 1
