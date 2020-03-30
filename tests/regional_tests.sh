0e7c120aaf24b57f9512b286b8dfc263be99c8ac
Sammy Fung <sammy@sammy.hk>
2020-03-30 14:28:17 +0800

    update spider test script

--- tests/regional_tests.sh
+++
@@ -1,9 +0,0 @@
-rm -f regional.csv
-scrapy crawl regional -t csv -o regional.csv --nolog
-line_count=`wc -l regional.csv | cut -c 7-8`
-if [ ${line_count} -eq " 0" ]
-then
-	exit 1
-else
-	exit 0
-fi
---
+++ tests/spider_test.sh
@@ -0,0 +1,38 @@
+if [ $# -ne 1 ]
+then
+    echo "Syntax: ${0} SPIDER_NAME"
+    exit 1
+else
+    SPIDER=${1}
+    scrapy list | grep ${SPIDER}
+    if [ $? -ne 0 ]
+    then
+        echo "Error: Spider ${SPIDER} is not found."
+        exit 1
+    fi
+fi
+
+OSTYPE=`uname`
+
+rm -f ${SPIDER}.csv
+scrapy crawl ${SPIDER} -t csv -o ${SPIDER}.csv --nolog
+
+if [ ${OSTYPE} = "Linux" ]
+then
+        line_count=`wc -l ${SPIDER}.csv | cut -f 1 -d ' '`
+        if [ ${line_count} = "0" ]
+        then
+                exit 1
+        else
+                exit 0
+        fi
+elif [ ${OSTYPE} = "Darwin" ]
+then
+    line_count=`wc -l ${SPIDER}.csv | cut -c 7-8`
+        if [ "${line_count}" = " 0" ]
+        then
+                exit 1
+        else
+                exit 0
+        fi
+fi
